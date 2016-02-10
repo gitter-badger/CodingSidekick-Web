2bbc143acfef77688dd808b4478563baac58cba3
unknown <csoler@TW-4B4M9Y1.ecardio.net>
2016-02-10 11:44:55 -0600

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
 var gulp = require('gulp');
 var config = require('./server/config/config');
-var glp = require('gulp-load-plugins')({lazy: true});
+var glp = require('gulp-load-plugins')({ lazy: true });
+var nodemon = glp.nodemon;
 
 gulp.task('styles', function () {
     return gulp
         .src(config.gulp.lessSrc)
         .pipe(glp.less())
-        .pipe(gulp.dest(config.gulp.lessDest));
+        .pipe(gulp.dest(config.gulp.lessDest))
+        .on('error', function () {
+            nodemon.emit('restart');
+        });
 });
 
 gulp.task('nodemon', function () {
-    glp.nodemon({
+    nodemon({
         script: 'server.js',
-        ext: 'js html',
-        tasks: ['watch']
+        ext: 'js html'
     });
 });
 
